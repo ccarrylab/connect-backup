88710fb88e4104befe876da244da98eb1493c5df
Cohen Carryl <cohen.carryl@gmail.com>
2023-03-24 11:01:21 -0400

    Update env.mk

--- env.mk
+++ env.mk
@@ -9,16 +9,16 @@ COMMIT=$(shell git rev-parse HEAD)
 #The name for the CFN stack to be deployed
 STACK-NAME=connect-backup-lambda
 #The aws profile to build your SAM package
-BUILD-PROFILE=sethkor
+BUILD-PROFILE=default
 #The aws profile to deploy the sam package.  This can be different to the BUILD-PROFILE if you want
 DEPLOY-PROFILE=sethkor
 EXE-NAME=connect-backup-lambda-test
 LAMBDA-NAME=ConnectBackup
-REGION=ap-southeast-2
+REGION=us-west-2
 #This is your existing AWS sam deployment bucket.  It is not created as part of this project and I have assumed it
 #already exists for you.  For more info, see the AWS SAM documentation
-SAM-BUCKET=sethkor-sam
+SAM-BUCKET=gts
 #This is the bucket that will be created for your backups
 BACKUP-BUCKET=sethkor-connect-backup-test
 #This is your AWS Connect instance id.  You can get this from the console
-CONNECT-INSTANCE-ID=946d3719-32f0-4ccc-a3de-713f52f6db7f
+CONNECT-INSTANCE-ID=02af995c-9efc-41f6-8b20-0effe08f2b5d
